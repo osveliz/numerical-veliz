a467db25068fd17b98dad706ea63c889cd1dbf86
osveliz <osveliz@miners.utep.edu>
2020-06-23 19:25:46 -0600

    Fibonacci Search
    Will add Golden-section soon

--- src/minimization/TernDichFibGold.pas
+++ src/minimization/TernDichFibGold.pas
@@ -5,15 +5,18 @@
  * @author Oscar Veliz
  **************************)
 program TernDichFibGold;
+uses math;
 const
-    eps = 10E-7;
+    eps = 10E-7; (*10^-6*)
     phi = (1+sqrt(5))/2.0;
     max = 75;
+
 (* A function f(x) to evaluate *)
 function f(x: real):real;
 begin
-    f:=x*x*x/3-x*x/2-x-1.0;
+    f := power(x,3)/3 - power(x,2)/2 - x - 1;
 end;
+
 (**
  * Ternary Search
  * @param a - left side of interval
@@ -32,7 +35,7 @@ begin
         c := (2.0*a+b)/3.0;
         d := (a+2.0*b)/3.0;
         writeln(itr,',',a,',',c,',',d,',',b);
-        if f(c) < f(d)  then
+        if f(c) < f(d) then
             b := d
         else
             a := c;
@@ -62,7 +65,7 @@ begin
         c := m - halfeps;
         d := m + halfeps;
         writeln(itr,',',a,',',c,',',d,',',b);
-        if f(c) < f(d)  then
+        if f(c) < f(d) then
             b := d
         else
             a := c;
@@ -72,10 +75,64 @@ begin
     dichotomous := m;
 end;
 
-(* Coming soon Fibonacci and Golden-section Search *)
+(**
+ * Fibonacci Search
+ * @param a - left side of interval
+ * @param b - right side of interval
+ * @return midpoint of shrunken [a,b]
+ *)
+function fibonacci(a,b: real):real;
+var
+    c,d,fc,fd : real;
+    itr, n : integer;
+    fib: array[0..max] of qword; (*unsigned 64-bit int*)
+begin
+	fib[0] := 1;
+	fib[1] := 1;
+	for n := 2 to max do
+	begin
+		fib[n] := fib[n-1] + fib[n-2];
+		if fib[n] >((b-a)/eps) then
+			break;
+	end;
+	writeln('n=',n,' fibn=',fib[n]);
+    c := a + fib[n-2]/fib[n]*(b-a);
+    d := a + fib[n-1]/fib[n]*(b-a);    
+    fc := f(c);
+    fd := f(d);    
+    writeln('n,a,c,d,b');
+    writeln('0,',a,',',c,',',d,',',b);
+    itr := 0;
+    for n := n-1 downto 2 do
+    begin
+        if(fc < fd) then
+        begin
+            b := d;
+            d := c;
+            fd := fc;
+            c := a + (fib[n-2]/fib[n])*(b-a);
+            fc := f(c);
+        end
+        else
+        begin
+            a := c;
+            c := d;
+            fc := fd;
+            d := a + (fib[n-1]/fib[n])*(b-a);
+            fd := f(d);
+        end;
+        inc(itr);
+        writeln(itr,',',a,',',c,',',d,',',b);
+    end;
+    writeln('iterations ',itr);
+    fibonacci := (c+d)/2;
+end;
+
+(* Coming soon Golden-section Search *)
 
 (* Main *)
 begin
   writeln('ternary ', ternary(1,2));
   writeln('dichotomous ', dichotomous(1,2));
+  writeln('fibonacci', fibonacci(1,2));
 end.
