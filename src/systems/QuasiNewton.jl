c566a5bba9cba0032d715addf1fce50301d13961
Oscar Veliz <osveliz@users.noreply.github.com>
2021-04-03 15:43:04 -0600

    Generalized Steffensen's Method

--- src/systems/QuasiNewton.jl
+++ src/systems/QuasiNewton.jl
@@ -29,7 +29,9 @@ Requires `d+1` starting vectors in X.
 # Examples
 ```julia-repl
 julia> Secant(transpose([1.0 1.0;1.0 2.0;1.5 2.0]))
-[1.6180339736070495, 1.6180339507485015]
+2-element Array{Float64,1}:
+ 1.6180339736070495
+ 1.6180339507485015
 ```
 """
 function Secant(X, eps::Float64 = 10^-6)
@@ -53,6 +55,55 @@ function Secant(X, eps::Float64 = 10^-6)
     X[:,end]
 end
 
+"""
+    steffJ(X, FX = F(X), n = size(X,1))
+
+Traub-Steffensen approximation of the Jacobian.
+Requires starting vector X.
+FX is optional in case F(X) was already computed for efficiency.
+# Examples
+```julia-repl
+julia> steffJ([1.5;2.0])
+2×2 Array{Float64,2}:
+ 2.25  -1.0
+ 1.0   -2.5
+```
+"""
+function steffJ(X,FX = F(X),n = size(X,1))
+    H = Diagonal(FX)
+    J = X #placeholder to set dims
+    for i = 1:n
+        J = hcat(J, F(X + H[:,i]) - FX)
+    end
+    J = J[1:end, 2:end] * inv(H) #removes placeholder before mult
+end
+
+"""
+    Steffensen(X, eps=10^-6)
+
+Traub-Steffensen Method for a system of nonlinear equations in [`F`](@ref).
+Requires starting vector X.
+# Examples
+```julia-repl
+julia> Steffensen([1.5;2.0])
+2-element Array{Float64,1}:
+ 1.6180339736216265
+ 1.6180339387805316
+```
+"""
+function Steffensen(X, eps::Float64 = 10^-6)
+    n = size(X,1)
+    #println(X) #uncomment to see itr
+    FX = F(X)
+    while(norm(FX) > eps)
+        J = steffJ(X,FX,n)
+        X = X - J \ FX
+        #println(X) #uncomment to see itr
+        FX = F(X)
+    end
+    X
+end
+
 
 """
 Coming Soon ... additional Quasi-Newton Methods
@@ -63,3 +114,4 @@ Main
 """
 
 println(Secant(transpose([1.0 1.0;1.0 2.0;1.5 2.0])))
+println(Steffensen([1.5;2.0]))
