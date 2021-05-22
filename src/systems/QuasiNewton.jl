b0d03161852952b413414fcbf2e933fd9a961abf
Oscar Veliz <osveliz@users.noreply.github.com>
2021-05-22 10:07:43 -0600

    Finite Difference Method

--- src/systems/QuasiNewton.jl
+++ src/systems/QuasiNewton.jl
@@ -12,7 +12,7 @@ Overwrite for your own testing, currently set to:
 x^2 - y - 1
 x - y^2 + 1
 
-# Examples
+# Example
 ```julia-repl
 julia> F([1.0; 2.0])
 [-2.0, -2.0]
@@ -26,7 +26,7 @@ F(X) = [X[1]^2 - X[2] - 1; X[1] - X[2]^2 + 1]
 
 Wolfe-Bittner Secant Method for the system of nonlinear equations in [`F`](@ref).
 Requires `d+1` starting vectors in X.
-# Examples
+# Example
 ```julia-repl
 julia> Secant(transpose([1.0 1.0;1.0 2.0;1.5 2.0]))
 2-element Array{Float64,1}:
@@ -61,7 +61,7 @@ end
 Traub-Steffensen approximation of the Jacobian.
 Requires starting vector X.
 FX is optional in case F(X) was already computed for efficiency.
-# Examples
+# Example
 ```julia-repl
 julia> steffJ([1.5;2.0])
 2×2 Array{Float64,2}:
@@ -83,7 +83,7 @@ end
 
 Traub-Steffensen Method for a system of nonlinear equations in [`F`](@ref).
 Requires starting vector X.
-# Examples
+# Example
 ```julia-repl
 julia> Steffensen([1.5;2.0])
 2-element Array{Float64,1}:
@@ -104,9 +104,57 @@ function Steffensen(X, eps::Float64 = 10^-6)
     X
 end
 
+"""
+    fdJ(X, FX = F(X), h = 10^-4, n = size(X,1))
+
+(Forward) Finite Difference approximation of the Jacobian.
+Requires starting vector X. Optional h and n.
+FX is optional in case F(X) was already computed for efficiency.
+# Example
+```julia-repl
+julia> fdJ([1.5;2.0])
+2×2 Array{Float64,2}:
+ 3.0001  -1.0
+ 1.0     -4.0001
+```
+"""
+function fdJ(X,FX = F(X),h::Float64 = 10^-4,n = size(X,1))
+    H = h * I(n)
+    J = X #placeholder to set dims
+    for i = 1:n
+        J = hcat(J, F(X + H[:,i]) - FX)
+    end
+    J = h^-1 * J[1:end, 2:end] #removes placeholder and divide by h
+end
 
 """
-Coming Soon ... additional Quasi-Newton Methods
+    FiniteDiff(X, h = 10^-4, eps = 10^-6)
+
+Generalized Finite-Difference Method for a system of nonlinear equations in [`F`](@ref).
+Requires starting vector X. Optional h and eps.
+# Example
+```julia-repl
+julia> FiniteDiff([1.5;2.0])
+2-element Array{Float64,1}:
+ 1.6180340699246842
+ 1.6180341416981485
+```
+"""
+function FiniteDiff(X,h::Float64 = 10^-4, eps::Float64 = 10^-6)
+    n = size(X,1)
+    #println(X) #uncomment to see itr
+    FX = F(X)
+    while(norm(FX) > eps)
+        J = fdJ(X,FX,h,n)
+        X = X - J \ FX
+        #println(X) #uncomment to see itr
+        FX = F(X)
+    end
+    X
+end
+
+"""
+Coming Soon ... Broyden's Method
 """
 
 """
@@ -115,3 +163,4 @@ Main
 
 println(Secant(transpose([1.0 1.0;1.0 2.0;1.5 2.0])))
 println(Steffensen([1.5;2.0]))
+println(FiniteDiff([1.5;2.0]))
